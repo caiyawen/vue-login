bbe2301e16168f4057a5dbe3c454ae3bc82be8b3
caiyawen <caiyawen@edusoho.com>
2017-08-29 13:07:29 +0800

    修复第二次连接数据库报错问题

--- server/server.js
+++ server/server.js
@@ -10,7 +10,32 @@ var bodyParser = require('body-parser');
 App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({ extended: false }));
 
-var connection = mysql.createConnection({
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("error", function(error) {
+            if (error instanceof Error) {
+                if (error.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.error(error.stack);
+                    console.log("Lost connection. Reconnecting...");
+
+                    initializeConnection(connection.config);
+                } else if (error.fatal) {
+                    throw error;
+                }
+            }
+        });
+    }
+
+    var connection = mysql.createConnection(config);
+
+    // Add handlers.
+    addDisconnectHandler(connection);
+
+    connection.connect();
+    return connection;
+}
+
+var connection = initializeConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '',
@@ -19,23 +44,26 @@ var connection = mysql.createConnection({
 
 //第二次请求有问题
 App.post('/login', function(req, res) {
-    connection.connect();
+    // connection.connect();
     // console.log(JSON.stringify(req.body));
     if (!req.body) return res.sendStatus(400);
     var nickname = req.body.nickname;
     var password = req.body.password;
     console.log(nickname, password);
+    var aaa;
     connection.query("SELECT * from user where nickname = ? and password = ?", [nickname, password],
         function(error, results, fields) {
             if (error) throw error;
             if (results[0]) {
                 console.log('The solution is: ', results[0]);
                 res.send(results[0]);
+                aaa = results[0];
             } else {
                 res.send('账号或密码错误');
             }
         });
-    connection.end();
+    // connection.end();
+    console.log(123);
 })
 
 
